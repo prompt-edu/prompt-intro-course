51fc9322554561828eb97bcd72ed04ccc94e3bfd
Maximilian Rapp <53191688+rappm@users.noreply.github.com>
2025-05-19 08:17:57 +0200

    Refactor import paths from niclasheun/prompt2.0 to ls1intum/prompt2/servers/core (#530)

--- servers/intro_course/utils/getQueries.go
+++ servers/intro_course/utils/getQueries.go
@@ -1,6 +1,6 @@
 package utils
 
-// import db "github.com/niclasheun/prompt2.0/db/sqlc"
+// import db "github.com/ls1intum/prompt2/servers/core/db/sqlc"
 
 // // Helper to get db.Queries from either provided (for transactions) or singleton
 // func GetQueries(providedQueries *db.Queries, singletonQueries *db.Queries) db.Queries {
